984fc86e72be1e8e38e937c6762595b1cff4c654
Sasha <sashikers@gmail.com>
2018-02-13 17:43:51 -0800

    backend: basic server set up

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+console.log(PORT);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
